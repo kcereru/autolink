df422cf93682820ed026c8a968dc8ca91772aca0
KC Maddever <kcereru@gmail.com>
2019-05-26 23:03:37 +1200

    fix: retry previous fix with major change to regex

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: autolink
 # about: Plugin to autolink #numbers in posts to the corresponding post in the topic
-# version: 1.0.0
+# version: 1.0.1
 # authors: KC Maddever (kcereru)
 # url: https://github.com/kcereru/autolink
 
@@ -12,14 +12,10 @@ after_initialize do
   DiscourseEvent.on(:post_created) do |post|
 
     # If post exists, replace with link, otherwise no changes
+    # Ignore numbers followed by a square bracket, they may be links
 
-    post.raw.gsub!(/#(\d*)[^\]]/) { |post_num|
-      if(specific_post(post_num.tr('#', ''), post.topic_id))
-        '[#' + post_num.tr('#', '') + '](' + specific_post(post_num.tr('#', ''), post.topic_id).full_url + ')'
-      else
-        post_num
-      end
-    }
+    r = /#(\d+)(?=[^\d\]]|$)/
+    post.raw.gsub!(r) { |m| m.gsub!($1, get_link(post, $1)) }
 
     post.save
   end
@@ -28,4 +24,12 @@ end
 
 def specific_post(post_num, topic_id)
   Post.find_by("topic_id = :topic_id AND post_number = :post_number", topic_id: topic_id, post_number: post_num)
+end
+
+def get_link(post, post_num)
+  if(specific_post(post_num, post.topic_id))
+    return '[' + post_num + '](' + specific_post(post_num, post.topic_id).full_url + ')'
+  else
+    return post_num
+  end
 end
